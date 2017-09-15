ee1967e01e9394a6b23bf46d3a2abc88480a823f
Jameson Yount <jyount@covermymeds.com>
2017-09-15 15:38:57 -0400

    adds admin uesr

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(email: 'admin@test.com', password: 'Testing1')
+User.create(email: 'admin@test.com', password: 'Testing1', admin: true)
