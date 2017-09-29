c6e87e61b05cfcdb18b0ea044129283998f76486
Jameson Yount <jyount@covermymeds.com>
2017-09-29 17:31:11 -0400

    comments out stuff that should not have been uncommented

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -1,13 +1,13 @@
 class Users::InvitationsController < Devise::InvitationsController
   before_action :authorize_admin, only: [:new, :create]
-
-  def new
-    super
-  end
-
-  def create
-    super
-  end
+  # 
+  # def new
+  #   super
+  # end
+  #
+  # def create
+  #   super
+  # end
 
   private
 
