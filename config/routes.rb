d41e93c596ebf7f5f1048d820b40e50ee7558e2f
Jameson Yount <jyount@covermymeds.com>
2017-10-02 17:21:35 -0400

    adds user and user view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  match '/users', to: 'users#index', via: 'get'
-
-  devise_for :users, :controllers => { :invitations => 'users/invitations' }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
+  devise_for :users, :controllers => { :invitations => 'users/invitations' }
+  resources :users, :only => [:index, :show]
 end
