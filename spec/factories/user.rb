73d4fd25e50c26bac2dc297b07ee45fba1894254
Jameson Yount <jyount@covermymeds.com>
2017-10-06 13:22:35 -0400

    adds model tests

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,7 +2,6 @@ FactoryGirl.define do
   factory :user do
     email { Faker::Internet.email }
     password 'testing'
-    admin false
     name { "#{Faker::Name.first_name} #{Faker::Name.last_name}" }
 
     trait :admin do
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-    it { is_expected.to validate_presence_of(:email) }
-    it { is_expected.to validate_presence_of(:password) }
-    it { is_expected.to validate_presence_of(:name) }
+  let(:user) { create(:user) }
+
+  it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_uniqueness_of(:email).ignoring_case_sensitivity }
+  it { is_expected.to validate_presence_of(:password) }
+  it { is_expected.to validate_presence_of(:name) }
+
+  it 'validates admin is false' do
+    expect(user.admin).to be(false)
+  end
+
+  it 'validates email format' do
+    user.email = 'invalid'
+    expect(user).not_to be_valid
+  end
 end
