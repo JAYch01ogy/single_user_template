31078392a4749822921c497824537c2eb9f162b7
Jameson Yount <jyount@covermymeds.com>
2017-10-13 16:09:41 -0400

    removes repetitive code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,15 +7,12 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = 'Successfully updated user.'
       redirect_to user_path
