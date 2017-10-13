76d73eb2dc754476c3d0f47e5ca8e0eeb8c6fbb2
Jameson Yount <jameson@Jamesons-MacBook-Pro.local>
2017-10-12 21:07:21 -0400

    adds user editing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,14 +5,26 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    find_user
   end
 
   def edit
+    find_user
+  end
+
+  def update
+    find_user
+    if @user.update(user_params)
+      flash[:notice] = 'Successfully updated user.'
+      redirect_to user_path
+    else
+      flash[:error] = 'User was not updated.'
+      redirect_to edit_user_path
+    end
   end
 
   def destroy
-    @user = User.find(params[:id])
+    find_user
     if @user.admin?
       flash[:error] = 'You cannot delete the admin user.'
       redirect_to users_path
@@ -23,4 +35,14 @@ class UsersController < ApplicationController
       end
     end
   end
+
+  private
+
+    def find_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:email, :name)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   devise_for :users, :controllers => { :invitations => 'users/invitations' }
-  resources :users, :only => [:index, :show, :edit, :destroy]
+  resources :users, :only => [:index, :show, :edit, :update, :destroy]
 end
